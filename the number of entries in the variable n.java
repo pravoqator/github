f03d7c3351beaa5b41ab1e5bd9d9dd25e8ba7add
Vladimir <79862841+pravoqator@users.noreply.github.com>
2021-03-02 15:31:10 +0200

    Update the number of entries in the variable n.java

---
+++ the number of entries in the variable n.java
@@ -0,0 +1,11 @@
+public class one {
+ public static void main(String[] args) {
+  int n;
+  n = 9;
+  if(n%2==0) {
+   System.out.println(n+" - chetnoe chislo");
+  } else {
+   System.out.println(n+" - nechetnoe chislo");
+  }
+ }
+}
